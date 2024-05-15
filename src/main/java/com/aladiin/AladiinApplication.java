1d0f9f5e2b3ace3b8d53ce3b92c9f184074a4623
dvlprkoji <koji4321@naver.com>
2024-05-15 14:42:49 +0900

    [#2] 프로젝트 생성

---
+++ src/main/java/com/aladiin/AladiinApplication.java
@@ -0,0 +1,13 @@
+package com.aladiin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AladiinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AladiinApplication.class, args);
+	}
+
+}
