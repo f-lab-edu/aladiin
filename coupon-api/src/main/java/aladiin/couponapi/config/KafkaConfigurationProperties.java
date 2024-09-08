725af4fcc572612cf542386d0fcce83dc2d14d61
dvlprkoji <koji4321@naver.com>
2024-09-08 21:00:16 +0900

    fix: Kafka Producer 설정 변경
    - 브로커 1개 > 3개 변경

--- coupon-api/src/main/java/aladiin/couponapi/config/KafkaConfigurationProperties.java
+++ coupon-api/src/main/java/aladiin/couponapi/config/KafkaConfigurationProperties.java
@@ -5,12 +5,14 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Getter @Setter
 @Component
 @ConfigurationProperties(prefix = "spring.kafka")
 public class KafkaConfigurationProperties {
     private String topic;
-    private String bootstrapServers;
+    private List<String> bootstrapServers;
 }
 
 
