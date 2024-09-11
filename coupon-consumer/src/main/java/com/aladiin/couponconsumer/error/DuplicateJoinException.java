73dcc7f986854c2c1f20ac98ba83a7125a990ee3
dvlprkoji <koji4321@naver.com>
2024-09-11 22:02:40 +0900

    feat: 이벤트 중복 참여 판단 로직 추가

---
+++ coupon-consumer/src/main/java/com/aladiin/couponconsumer/error/DuplicateJoinException.java
@@ -0,0 +1,5 @@
+package com.aladiin.couponconsumer.error;
+
+public class DuplicateJoinException extends RuntimeException {
+
+}
--- coupon-consumer/src/main/java/com/aladiin/couponconsumer/service/EventService.java
+++ coupon-consumer/src/main/java/com/aladiin/couponconsumer/service/EventService.java
@@ -1,11 +1,20 @@
 package com.aladiin.couponconsumer.service;
 
+import com.aladiin.couponconsumer.domain.EventParticipants;
+import com.aladiin.couponconsumer.error.DuplicateJoinException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class EventService {
-    public void checkDuplicateJoin(Long memberId, Long eventId, String eventDate) {
 
+    private final EventParticipants eventParticipants;
+
+    public void checkDuplicateJoin(Long memberId, Long eventId, String eventDate) throws DuplicateJoinException {
+        if (eventParticipants.containsMember(memberId, eventId, eventDate)) {
+            throw new DuplicateJoinException();
+        }
     }
 
     public void join(Long memberId, Long eventId, String eventDate) {
